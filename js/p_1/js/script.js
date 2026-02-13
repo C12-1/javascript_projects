25e3019348fadde8d650458344b6ec36d81898f3
ctwelve <hisokaff88@gmail.com>
2026-02-13 18:20:51 +0100

    adding showdate function

--- js/p_1/js/script.js
+++ js/p_1/js/script.js
@@ -23,7 +23,7 @@ function getTotal() {
     }
 };
 let dataPro;
-if (localStorage.getItem("locale_data") == false) {
+if (localStorage.getItem("locale_data") == null) {
 
     dataPro =[];
 }
@@ -37,22 +37,47 @@ submit.onclick = function() {
         taxes:taxes.value,
         ads:ads.value,
         discount:discount.value,
-        total:total.value,
+        total:total.innerHTML,
         count:count.value,
         category:category.value,
-    };clearData
-    dataPro += newPro;
-    localStorage.setItem("locale_data" , JSON.stringify(dataPro))
+    };
+    dataPro.push(newPro);
+    localStorage.setItem("locale_data" , JSON.stringify(dataPro));
     clearData();
+    showData();
 }
 
 function clearData() {
-    price.value = ""
-    title.value = ""
-    taxes.value= ""
-    ads.value= ""
-    discount.value= ""
-    total.value= ""
-    count.value= ""
-    category.value= ""
+    price.value = "";
+    title.value = "";
+    taxes.value= "";
+    ads.value= "";
+    discount.value= "";
+    count.value= "";
+    category.value= "";
+    total.innerHTML= "";
+    total.style.background = "#a00d02"
 }
+
+function showData() {
+    let table = "";
+    for (let i = 0 ; i < dataPro.length ; i++) {
+        
+        table +=`
+     <tr>
+        <td>${i}</td>
+        <td>${dataPro[i].title}</td>
+        <td>${dataPro[i].price}</td>
+        <td>${dataPro[i].taxes}</td>
+        <td>${dataPro[i].ads}</td>
+        <td>${dataPro[i].discount}</td>
+        <td>${dataPro[i].total}</td>
+        <td>${dataPro[i].category}</td>
+        <td><button type="submit"id="update">update</button></td>
+        <td><button type="submit"id="delete">delete</button></td>
+    </tr>`;
+    }
+    document.getElementById("tbody").innerHTML = table
+    
+}
+showData();
