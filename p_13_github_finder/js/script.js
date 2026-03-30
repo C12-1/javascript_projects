632393749dab655750771a72dabd625b1cf43933
C-12 <hisokaff88@gmail.com>
2026-03-30 16:24:12 +0100

    completing the css and html and some js in project 13 github finder

---
+++ p_13_github_finder/js/script.js
@@ -0,0 +1,63 @@
+const searchInput = document.getElementById("search");
+const searchBtn = document.getElementById("search-btn");
+const profileContainer = document.getElementById("profile-container");
+const errorContainer = document.getElementById("error-container");
+const avatar = document.getElementById("avatar");
+const nameElement = document.getElementById("name");
+const usernameElement = document.getElementById("username");
+const bioElement = document.getElementById("bio");
+const locationElement = document.getElementById("location");
+const joinedDateElement = document.getElementById("joined-date");
+const profileLink = document.getElementById("profile-link");
+const followers = document.getElementById("followers");
+const following = document.getElementById("following");
+const repos = document.getElementById("repos");
+const companyElement = document.getElementById("company");
+const blogElement = document.getElementById("blog");
+const twitterElement = document.getElementById("twitter");
+const companyContainer = document.getElementById("company-container");
+const blogContainer = document.getElementById("blog-container");
+const twitterContainer = document.getElementById("twitter-container");
+const reposContainer = document.getElementById("repos-container");
+
+
+
+searchInput.addEventListener("keypress" , (e) => {
+    if(e.key === "Enter") {
+    searchUser() ;
+    return;
+    }
+});
+searchBtn.addEventListener("click" , searchUser);
+
+async function searchUser(){
+    const username  = searchInput.value.trim();
+    if (!username) return alert("please enter a username !!");
+    try {
+        profileContainer.classList.add("hidden");
+        errorContainer.classList.add("hidden");
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        if(!response.ok) throw new Error("user no found !!")
+        const data = await response.json();
+        console.log(data)
+
+        displayUserData(data);
+    }
+    catch (error) {
+        showError();
+    }
+}
+
+function displayUserData(data){
+
+}
+
+function showError() {
+    errorContainer.classList.remove("hidden");
+    profileContainer.classList.add("hidden");
+}
+
+
+searchInput.value = "C12-1";
+
+searchUser();
