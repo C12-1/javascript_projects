4a1756a20ccabc760b18bbca25be3e5caa112cc0
C-12 <hisokaff88@gmail.com>
2026-04-02 15:28:17 +0100

    completing the github finder project

--- p_13_github_finder/js/script.js
+++ p_13_github_finder/js/script.js
@@ -39,9 +39,9 @@ async function searchUser(){
         const response = await fetch(`https://api.github.com/users/${username}`);
         if(!response.ok) throw new Error("user no found !!")
         const data = await response.json();
-        console.log(data)
 
         displayUserData(data);
+        fetchRepositories(data.repos_url);
     }
     catch (error) {
         showError();
@@ -49,7 +49,102 @@ async function searchUser(){
 }
 
 function displayUserData(data){
+    avatar.src = data.avatar_url;
+    nameElement.textContent = data.name || data.login;
+    usernameElement.textContent = `@${data.login}`;
+    bioElement.textContent = data.bio || "No bio available" ;
+    locationElement.textContent = data.location || "Not specified";
+    joinedDateElement.textContent = formatDate(data.created_at) ;
 
+    profileLink.href = data.html_url;
+    followers.textContent = data.followers;
+    following.textContent = data.following;
+    repos.textContent = data.public_repos;
+
+    companyElement.textContent = data.company || "Not specified";
+    
+    if (data.blog) {
+        blogElement.textContent = data.blog;
+        blogElement.href = data.blog.startsWith("http") ? data.blog : `https://${data.blog}`;
+      } else {
+        blogElement.textContent = "No website";
+        blogElement.href = "#";
+        blogElement.target = "_self";
+      }
+    blogContainer.style.display = "flex";
+    
+    if (data.twitter_username) {
+        twitterElement.textContent = `@${data.twitter_username}`;
+        twitterElement.href = `https://twitter.com/${data.twitter_username}`;
+    } else {
+        twitterElement.textContent = "No Twitter";
+        twitterElement.href = "#";
+        twitterElement.target = "_self";
+    }
+    twitterElement.style.display = "flex";
+    // this to show the profile 
+    profileContainer.classList.remove("hidden");
+}
+
+
+async function fetchRepositories(reposUrl){
+    reposContainer.innerHTML = `<div class="loading-repos">Loading repositories...</div>`;
+
+    try{
+        const response = await fetch(reposUrl + "?pre_page=15");
+        const data = await response.json();
+        displayRepos(data);
+    }
+    catch(error){
+        reposContainer.innerHTML = `<div class="no-repos">${error.message}</div>`;
+    }
+}
+
+function displayRepos(repos){
+    if (repos.length === 0){
+        reposContainer.innerHTML = "<div class='no-repos'>No repositories found</div>";
+        return;
+    }
+    reposContainer.innerHTML ="";
+    repos.forEach((repo) => {
+        const repoCard = document.createElement("div");
+        repoCard.className = "repo-card";
+        const updatedAt = formatDate(repo.updated_at);
+        repoCard.innerHTML = `
+            <a href="${repo.html_url}" target="_blank" class="repo-name">
+                <i class="fas fa-code-branch"></i> ${repo.name}
+            </a>
+            <p class="repo-description">${repo.description || "No description available"}</p>
+            <div class="repo-meta">
+                ${
+                    repo.language
+                        ? `
+                    <div class="repo-meta-item">
+                        <i class="fas fa-circle"></i> ${repo.language}
+                    </div>
+                    `
+                        : ""
+                }
+                <div class="repo-meta-item">
+                <i class="fas fa-star"></i> ${repo.stargazers_count}
+                </div>
+                <div class="repo-meta-item">
+                <i class="fas fa-code-fork"></i> ${repo.forks_count}
+                </div>
+                <div class="repo-meta-item">
+                <i class="fas fa-history"></i> ${updatedAt}
+                </div>
+            </div>
+            `;
+        reposContainer.appendChild(repoCard);
+    })
+}
+function formatDate(dateString){
+    return new Date(dateString).toLocaleDateString("en-us" , {
+        year : "numeric",
+        month : "short",
+        day : "numeric",
+    });
 }
 
 function showError() {
